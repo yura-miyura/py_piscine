5c63bfde5d7408b543ef23feff128a7282caa337
Yurii Artym <140413112+yura-miyura@users.noreply.github.com>
2026-03-04 08:15:22 +0000

    py_01 started

---
+++ py_01/ex0/ft_garden_intro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def ft_garden_info() -> None:
+    print("=== Welcome to My Garden ===")
+    name = "Rose"
+    height = 25
+    age = 30
+    print(f"Plant: {name}\nHeight: {height}cm\nAge: {age} days\n")
+    print("=== End of Program ===")
+
+if __name__ == "__main__":
+    ft_garden_info()
