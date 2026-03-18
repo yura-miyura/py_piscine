b2a27e30683edef4166d2779b8dd3b2368e627c4
Yurii Artym <140413112+yura-miyura@users.noreply.github.com>
2026-03-18 12:07:34 +0000

    py03, ex0 and ex1 done

--- py_03/ex0/ft_command_quest.py
+++ py_03/ex0/ft_command_quest.py
@@ -1,3 +1,29 @@
 #!/usr/bin/env python3
 
+import sys
 
+
+def print_args(ac: int, av: list) -> None:
+    if ac == 0:
+        return
+    print_args(ac - 1, av)
+    print(f"Argument {ac}: {av[ac]}")
+
+
+def ft_command_quest() -> None:
+    print("=== Command Quest ===")
+    av: list = sys.argv
+    ac: int = len(av)
+    if (ac > 1):
+        if "42" in av:
+            av[0] = "\\_".join(av[0].split('_'))
+        print(f"Program name: {av[0]}")
+        print(f"Arguments received: {ac - 1}")
+        print_args(ac - 1, av)
+    else:
+        print("No arguments provided!")
+    print(f"Total arguments: {ac}\n")
+
+
+if __name__ == "__main__":
+    ft_command_quest()
---
+++ py_03/ex1/ft_score_analytics.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+
+def add_scores(scores: list) -> list[int]:
+    scores.pop(0)
+    scores_int: list[int] = []
+    score_int: int
+    for score in scores:
+        try:
+            score_int = int(score)
+            scores_int.append(score_int)
+        except ValueError:
+            raise ValueError(f"oops, I typed ’{score}’ instead of ’1000’")
+    return scores_int
+
+
+def ft_score_analytics() -> None:
+    print("=== Player Score Analytics ===")
+    try:
+        scores: list[int] = add_scores(argv)
+    except ValueError as e:
+        print(e)
+    else:
+        players: int = len(scores)
+        if players == 0:
+            print("No scores provided.",
+                  "Usage: python3 ft_score_analytics.py",
+                  "<score1> <score2> ...")
+        else:
+            print(f"Score processed: {scores}")
+            print(f"Total players: {players}")
+            print(f"Total score: {sum(scores)}")
+            print(f"Average score: {sum(scores) / players}")
+            print(f"High score: {max(scores)}")
+            print(f"Low score: {min(scores)}")
+            print(f"Score range: {max(scores) - min(scores)}")
+    print()
+
+
+if __name__ == "__main__":
+    ft_score_analytics()
